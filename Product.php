7e7ec6cde249849e5a064ae016a73088ca11cd9e
Miguel Martín <miguelmc.es@gmail.com>
2022-11-07 17:30:51 +0100

    e

---
+++ Product.php
@@ -0,0 +1,28 @@
+<?php
+// Fichero que simula el modelo con datos
+
+class Product
+{
+    const PRODUCTS = [
+        array(1, 'Cortacesped'),
+        array(2, 'Pizarra'),
+        array(3, 'Billar'),
+        array(4, 'Dardos'),
+    ];
+
+    function __construct()
+    { /*cons vacio*/
+    }
+
+    // Devuelve todos los productos
+    public static function all()
+    {
+        return Product::PRODUCTS;
+    }
+
+    // Devolver un producto en particular
+    public function find($id)
+    {
+        return Product::PRODUCTS[$id-1];
+    }
+}
