768e6992d6640fa78d603b93706741d16692d942
Miguel Martín <miguelmc.es@gmail.com>
2022-11-23 18:20:35 +0100

    mvc 06

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -3,12 +3,14 @@
 namespace App\Controllers;
 
 include "../Product.php"; 
+//include_once "./vendor/autoload.php";
+use Dompdf\Dompdf;
 
 class ProductController
 {
     function __construct()
     {
-        echo "<br>Constructor clase PRODUCTCONTROLLER";
+        //echo "<br>Constructor clase PRODUCTCONTROLLER";
     }
 
     function index()
@@ -27,5 +29,14 @@ class ProductController
         $id = $_GET["id"];
         $product = \Product::find($id);
         require "../app/views/show.php";
+    }
+
+    function pdf(){
+        $dompdf = new Dompdf();
+        $dompdf->loadHtml('<h1>Hola mundo</h1><br><a href="https://parzibyte.me/blog">By Parzibyte</a>');
+        $dompdf->render();
+        header("Content-type: application/pdf");
+        header("Content-Disposition: inline; filename=documento.pdf");
+        $dompdf->stream();
     }
 }
