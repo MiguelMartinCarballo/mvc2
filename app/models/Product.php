6e8d16252295cc675cf46354b873dcfea167717d
Miguel Martín <miguelmc.es@gmail.com>
2022-11-27 21:17:08 +0100

    kjfads

--- app/models/Product.php
+++ app/models/Product.php
@@ -10,6 +10,11 @@ require_once '../core/Model.php';
 */
 class User extends Model
 {
+    public function __construct()
+    {
+        $this->birthdate = DateTime::createFromFormat('Y-m-d', $this->birthdate);
+    }
+
     public static function all(){ }
     public static function find($id){ }
     public function insert(){ }
